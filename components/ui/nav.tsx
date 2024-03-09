f83ad8dd5a56f27e482f10f128fd9423b9cff4c4
TejasAnand <anandtejas455@gmail.com>
2024-03-08 20:18:25 -0500

    added Theme Toggle

---
+++ components/ui/nav.tsx
@@ -0,0 +1,14 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+export default function Nav() {
+  return <h1>Hello World</h1>;
+}
