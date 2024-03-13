801a05f5b12799c922da674dd39198e32d807618
TejasAnand <anandtejas455@gmail.com>
2024-03-13 01:46:06 -0400

    fixed links

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -8,27 +8,33 @@ export default function Footer() {
     <div className="footerContainer">
       <div className="socials">
         <h1 className="footerText">Be kind, work hard, embrace adversity</h1>
-        <Image
-          className="insta"
-          src={instagramIcon}
-          alt="instagram"
-          width={96}
-          height={96}
-        ></Image>
-        <Image
-          className="insta"
-          src={githubIcon}
-          alt="GitHub"
-          width={64}
-          height={64}
-        ></Image>
-        <Image
-          className="insta"
-          src={mailIcon}
-          alt="Email"
-          width={64}
-          height={64}
-        ></Image>
+        <a href="https://www.instagram.com/tejasanand_/">
+          <Image
+            className="insta"
+            src={instagramIcon}
+            alt="instagram"
+            width={96}
+            height={96}
+          ></Image>
+        </a>
+        <a href="https://github.com/tejasanand">
+          <Image
+            className="insta"
+            src={githubIcon}
+            alt="GitHub"
+            width={64}
+            height={64}
+          ></Image>
+        </a>
+        <a href="mailto:tsanand@uwaterloo.ca">
+          <Image
+            className="insta"
+            src={mailIcon}
+            alt="Email"
+            width={64}
+            height={64}
+          ></Image>
+        </a>
       </div>
     </div>
   );
