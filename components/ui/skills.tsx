42a8b2010298338b3d331ca7432c269eb27017af
TejasAnand <anandtejas455@gmail.com>
2024-03-21 00:32:01 -0400

    add db stuff

--- components/ui/skills.tsx
+++ components/ui/skills.tsx
@@ -11,6 +11,13 @@ import {
 import Image from "next/image";
 
 import pythonSvg from "@/public/python.svg";
+import javascriptSvg from "@/public/javascript.svg";
+import typescriptSvg from "@/public/typescript.svg";
+import csvg from "@/public/c.svg";
+import perlSvg from "@/public/perl.svg";
+import sqlSvg from "@/public/mysql.svg";
+import mongoSvg from "@/public/mongodb.svg";
+import postgresqlSvg from "@/public/postgresql.svg";
 
 export default function Skills() {
   return (
@@ -26,11 +33,34 @@ export default function Skills() {
         <TableRow>
           <TableCell className="font-medium">Programming Languages</TableCell>
           <TableCell className="skillBox text-right">
-            <Image src={pythonSvg} alt="Python" height={50} width={50}></Image>
-            <Image src={pythonSvg} alt="Python" height={50} width={50}></Image>
-            <Image src={pythonSvg} alt="Python" height={50} width={50}></Image>
-            <Image src={pythonSvg} alt="Python" height={50} width={50}></Image>
-            <Image src={pythonSvg} alt="Python" height={50} width={50}></Image>
+            <Image src={pythonSvg} alt="Python" height={55} width={55}></Image>
+            <Image
+              src={javascriptSvg}
+              alt="JavaScript"
+              height={40}
+              width={40}
+            ></Image>
+            <Image
+              src={typescriptSvg}
+              alt="Typescript"
+              height={35}
+              width={35}
+            ></Image>
+            <Image src={csvg} alt="C" height={35} width={35}></Image>
+            <Image src={perlSvg} alt="Perl" height={40} width={40}></Image>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Databases</TableCell>
+          <TableCell className="skillBox text-right">
+            <Image src={sqlSvg} alt="JavaScript" height={40} width={40}></Image>
+            <Image
+              src={mongoSvg}
+              alt="Typescript"
+              height={35}
+              width={35}
+            ></Image>
+            <Image src={postgresqlSvg} alt="C" height={35} width={35}></Image>
           </TableCell>
         </TableRow>
       </TableBody>
